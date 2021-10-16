9b550775cefae7732638db1c294fe39fc444c60e
Luka Simić <git@kocka.tech>
2021-10-16 13:19:25 +0200

    Binarization user implementation demo.

--- user_demo.py
+++ user_demo.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import cv2
+import numpy as np
 from testbench import Testbench
 
 Testbench.author('Petar Petrović')
@@ -25,7 +27,15 @@ def is_even(n: int) -> bool:
     return n & 1 == 0
 
 
+def binarization(image: np.ndarray) -> np.ndarray:
+    threshold = 128 # 127 for 33% correctness, 129 for 66% correctness
+    image_grayscale: np.ndarray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    _, image_threshold = cv2.threshold(image_grayscale, threshold, 255, cv2.THRESH_BINARY)
+    return image_threshold
+
+
 if __name__ == '__main__':
     Testbench(fibonacci)
     Testbench(factorial)
     Testbench(is_even)
+    Testbench(binarization)
