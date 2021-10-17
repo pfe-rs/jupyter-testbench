a7645daa21458388222500387600e03cccb8861c
Filip Parag <filip@parag.rs>
2021-10-17 16:40:27 +0200

    remove examples

--- user_demo.py
+++ user_demo.py
@@ -1,45 +1,10 @@
 #!/usr/bin/env python
 
+import numpy as np
 from testbench import Testbench
 
 Testbench.author('Petar Petrović')
 
-
-def fibonacci(n: int) -> int:
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
-
-def factorial(n: int) -> int:
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-def is_even(n: int) -> bool:
-    return n & 1 == 0
-
-
-import cv2
-import numpy as np
-def binarization(image: np.ndarray) -> np.ndarray:
-    # 127 for 33% correctness, 129 for 66% correctness
-    threshold = 128
-    image_grayscale: np.ndarray = cv2.cvtColor(
-        image, cv2.COLOR_RGB2GRAY
-    )
-    _, image_threshold = cv2.threshold(
-        image_grayscale, 
-        threshold, 255, 
-        cv2.THRESH_BINARY
-    )
-    return image_threshold
-    
 
 def is_prime(n: int) -> bool:
     if n < 2:
@@ -50,12 +15,13 @@ def is_prime(n: int) -> bool:
             return False
     return True
 
+
 def heart_beats(name: str) -> int:
-    data = np.fromfile(name, dtype = int)
-    signal=np.diff(data)
+    data = np.fromfile(name, dtype=int)
+    signal = np.diff(data)
     br = 0
     i = 0
-    while i<np.size(signal)-1:
+    while i < np.size(signal)-1:
         if signal[i] > 500000:
             br = br+1
             i = i + 100
@@ -65,9 +31,5 @@ def heart_beats(name: str) -> int:
 
 
 if __name__ == '__main__':
-    Testbench(fibonacci)
-    Testbench(factorial)
-    Testbench(is_even)
-    Testbench(binarization)
     Testbench(is_prime)
     Testbench(heart_beats)
