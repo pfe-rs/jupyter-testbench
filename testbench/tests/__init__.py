496a7b323e2510606c826533303cd1a75b5a8fce
Marija Nedeljković <nedeljkovicmajaa@gmail.com>
2021-10-17 14:18:51 +0200

    Update __init__.py

--- testbench/tests/__init__.py
+++ testbench/tests/__init__.py
@@ -3,3 +3,4 @@ from .fibonacci import test_fibonacci
 from .factorial import test_factorial
 from .is_even import test_is_even
 from .is_prime import test_is_prime
+from .heart_beats import heart_beats
