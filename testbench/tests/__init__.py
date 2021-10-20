fed22ec47f38794a42bdd1aca335a538f777a66d
Aleksa Racic <racicaleksa7@gmail.com>
2021-10-20 16:46:33 +0200

    Bux fixes: test_image_loader

--- testbench/tests/__init__.py
+++ testbench/tests/__init__.py
@@ -1,6 +1,5 @@
-from .binarization import test_binarization
-from .fibonacci import test_fibonacci
 from .factorial import test_factorial
 from .is_even import test_is_even
 from .image_loader import test_image_loader
 from .return_model1 import test_return_model1
+from .callable_classes_test import test___call__
