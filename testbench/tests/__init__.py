76d5f9d067c543d39eb93efa37ea01ee243d51dd
jelena3214 <37124826+jelena3214@users.noreply.github.com>
2021-10-20 16:30:27 +0200

    dodata funkcija return_model1

--- testbench/tests/__init__.py
+++ testbench/tests/__init__.py
@@ -3,3 +3,4 @@ from .fibonacci import test_fibonacci
 from .factorial import test_factorial
 from .is_even import test_is_even
 from .image_loader import test_image_loader
+from .return_model1 import test_return_model1
