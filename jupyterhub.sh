1399c84b6319b5ff4674bda2c288ca5e41029296
Luka Simić <git@kocka.tech>
2021-10-20 14:38:17 +0200

    Don't link testbench/tests/datasets, copy instead.

--- jupyterhub.sh
+++ jupyterhub.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
-users_dir="$HOME/users"
-python_libs="$(python3 -m site --user-site)"
-user_libs="$(echo "$python_libs" | sed "s:$HOME::")"
+users_dir="/home"
+cd /tmp # If we're in the testbench directory this 
 
 dataset_users="$dir/datasets"
 dataset_bench="$dir/testbench/tests/datasets"
@@ -13,25 +12,6 @@ for user in $(find "$users_dir" -maxdepth 1 -mindepth 1 -type d); do
         echo "delete $user/dataset"
         rm -f "$user/dataset"
     fi
-    echo "link $dataset_users -> $user/dataset"
+    echo "link $user/dataset -> $dataset_users"
     ln -s "$dataset_users" "$user/dataset"
-
-    if [ -e "$user$user_libs" ]; then
-        echo "delete $user$user_libs"
-        rm -rf "$user$user_libs"
-    else
-        parent="$(dirname "$user$user_libs")"
-        echo "mkdir $parent"
-        mkdir -p "$parent"
-    fi
-    echo "link $python_libs -> $user$user_libs"
-    ln -s "$python_libs" "$user$user_libs"
 done
-
-if [ -L "$user/dataset" ]; then
-    echo "delete $python_libs/testbench/tests/datasets"
-    rm -f "$python_libs/testbench/tests/datasets"
-fi
-echo "link $dataset_bench -> $python_libs/testbench/tests/datasets"
-ln -s "$dataset_bench" "$python_libs/testbench/tests/datasets"
-
