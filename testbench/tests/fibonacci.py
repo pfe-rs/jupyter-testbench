a61131ea891c7afb3a56b7c575e8b62b0bdce5a5
Aleksa Racic <racicaleksa7@gmail.com>
2021-10-20 17:29:16 +0200

    Izbrisani test primeri da se validator ne ljuti

--- testbench/tests/fibonacci.py
+++
@@ -1,8 +0,0 @@
-def test_fibonacci(bench: 'Testbench'):
-
-    bench.assert_eq(bench.function(0), 0)
-    bench.assert_eq(bench.function(1), 1)
-    bench.assert_eq(bench.function(2), 1)
-    bench.assert_eq(bench.function(3), 2)
-    bench.assert_eq(bench.function(5), 5)
-    bench.assert_eq(bench.function(20), 6765)
--- testbench/tests/is_even.py
+++
@@ -1,8 +0,0 @@
-def test_is_even(bench: 'Testbench'):
-
-    bench.assert_eq(bench.function(0), True)
-    bench.assert_eq(bench.function(1), False)
-    bench.assert_eq(bench.function(2), True)
-    bench.assert_eq(bench.function(3), False)
-    bench.assert_eq(bench.function(2468), True)
-    bench.assert_eq(bench.function(12345), False)
