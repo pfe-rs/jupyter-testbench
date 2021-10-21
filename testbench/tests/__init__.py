a14e1cbcff56ce363374b4337759fab7c66ea82a
natasa-jovanovic <57871141+natasa-jovanovic@users.noreply.github.com>
2021-10-21 14:15:31 +0200

    Update __init__.py

--- testbench/tests/__init__.py
+++ testbench/tests/__init__.py
@@ -1,4 +1,1 @@
-from .binarization import test_binarization
-from .fibonacci import test_fibonacci
-from .factorial import test_factorial
-from .is_even import test_is_even
+from .k_means import test_k_means
