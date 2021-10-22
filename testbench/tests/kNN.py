c08581b32d4712e5d106f4c8a7a08bc34f6e45db
natasa-jovanovic <57871141+natasa-jovanovic@users.noreply.github.com>
2021-10-22 20:57:15 +0200

    Add files via upload

---
+++ testbench/tests/kNN.py
@@ -0,0 +1,34 @@
+import numpy as np
+number_of_tests = 5
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def test_kNN(bench: 'Testbench'):
+
+    name = "testbench/tests/datasets/kNN/data_after_pca.csv"
+
+    df = pd.read_csv(name)
+    clean_df = pd.DataFrame()
+
+    # No need to be cleaned
+    columns = ["PC1", "PC2", "NObeyesdad"]
+    data = []
+
+    data.append(df["PC1"])
+    data.append(df["PC2"])
+    data = np.array(data)
+    data = np.transpose(data)
+
+    true_labels = np.array(df["NObeyesdad"])
+
+    X_train, X_test, y_train, y_test = train_test_split(data, true_labels, test_size=0.30)
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    predicted_labels = bench.function(X_test, X_train, y_train)
+
+    for i in range(len(y_test)):
+
+        bench.assert_eq(predicted_labels[i], y_test[i])
