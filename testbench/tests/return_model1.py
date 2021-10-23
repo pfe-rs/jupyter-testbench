c1c65df7c81f5228868362ed03507365c856dec6
jelena3214 <37124826+jelena3214@users.noreply.github.com>
2021-10-23 14:57:04 +0200

    ispravka

--- testbench/tests/return_model1.py
+++ testbench/tests/return_model1.py
@@ -1,13 +1,23 @@
 from io import StringIO
 import sys
 
-expected = """Net(
-  (conv1): Conv2d(3, 16, kernel_size=(5, 5), stride=(1, 1))
-  (pool): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-  (conv2): Conv2d(16, 32, kernel_size=(5, 5), stride=(1, 1))
-  (fc1): Linear(in_features=5408, out_features=120, bias=True)
-  (fc2): Linear(in_features=120, out_features=84, bias=True)
-  (fc3): Linear(in_features=84, out_features=10, bias=True)
+expected = """Sequential(
+  (0): Conv2d(3, 16, kernel_size=(5, 5), stride=(1, 1))
+  (1): BatchNorm2d(16, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+  (2): ReLU()
+  (3): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+  (4): Conv2d(16, 32, kernel_size=(5, 5), stride=(1, 1))
+  (5): BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+  (6): ReLU()
+  (7): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+  (8): Flatten(start_dim=1, end_dim=-1)
+  (9): Linear(in_features=89888, out_features=120, bias=True)
+  (10): BatchNorm1d(120, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+  (11): ReLU()
+  (12): Linear(in_features=120, out_features=84, bias=True)
+  (13): BatchNorm1d(84, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+  (14): ReLU()
+  (15): Linear(in_features=84, out_features=10, bias=True)
 )
 """
 
