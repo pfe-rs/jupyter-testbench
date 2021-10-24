1e0f9c2d1af056d8dd227266b359825fdb420d4b
jelena3214 <37124826+jelena3214@users.noreply.github.com>
2021-10-24 22:55:17 +0200

    try

--- testbench/tests/return_fashioncnn.py
+++ testbench/tests/return_fashioncnn.py
@@ -1,33 +1,31 @@
-from io import StringIO
-import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from importlib.resources import contents, path
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .. import Testbench
 
-expected = """Sequential(
-  (0): Conv2d(1, 32, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-  (1): BatchNorm2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-  (2): ReLU()
-  (3): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-  (4): Conv2d(32, 64, kernel_size=(3, 3), stride=(1, 1))
-  (5): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-  (6): ReLU()
-  (7): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
-  (8): Flatten(start_dim=1, end_dim=-1)
-  (9): Linear(in_features=2304, out_features=600, bias=True)
-  (10): Dropout2d(p=0.25, inplace=False)
-  (11): Linear(in_features=600, out_features=120, bias=True)
-  (12): Linear(in_features=120, out_features=10, bias=True)
+def return_fashioncnn() -> nn.Sequential:
+    return nn.Sequential(
+    nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+    nn.BatchNorm2d(32),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    
+    nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+    nn.BatchNorm2d(64),
+    nn.ReLU(),
+    nn.MaxPool2d(2),
+    nn.Flatten(),
+
+    nn.Linear(in_features=64*6*6, out_features=600),
+    nn.Dropout2d(0.25),
+    nn.Linear(in_features=600, out_features=120),
+    nn.Linear(in_features=120, out_features=10),
 )
-"""
 
 def test_return_fashioncnn(bench: 'Testbench'):
-
-  old_stdout = sys.stdout
-
-  result = StringIO()
-  sys.stdout = result
-
-  print(bench.function())
-
-  result_string = result.getvalue()
-
-  sys.stdout = old_stdout
-  assert(expected == result_string)
+    if bench.function is None:
+        return
+    bench.assert_expr(repr(return_fashioncnn()) == repr(bench.function()))
