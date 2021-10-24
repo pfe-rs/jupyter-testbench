739183242b6ea3a9a85b5fa87d6200511a242731
jelena3214 <37124826+jelena3214@users.noreply.github.com>
2021-10-25 01:44:16 +0200

    fix

--- testbench/tests/__init__.py
+++ testbench/tests/__init__.py
@@ -1,5 +1,4 @@
 from .image_loader import test_image_loader
-from .return_model1 import test_return_model1
 from .training_loop import test_training_loop
 from .return_fashioncnn import test_return_fashioncnn
 from .getClipImage import test_getClipImage
